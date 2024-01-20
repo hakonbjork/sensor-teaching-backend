2e427c821b2d4ee5ca9f07f68b8f4b5c3aacdca8
Håkon Bjørkum <hakon1307@gmail.comm>
2024-01-20 18:36:20 +0100

    create dummy data processing and new endpoint

--- api/views.py
+++ api/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from dataprocessing.services import fetch_and_process_number
+
 @api_view(['GET'])
 def hello_world(request):
     return Response({'message': 'Hello, world!'})
+
+@api_view(['GET'])
+def number(request):
+    number = fetch_and_process_number()
+    return Response({'number': number})
---
+++ dataprocessing/services.py
@@ -0,0 +1,10 @@
+
+def get_number():
+    return 42
+
+def process_number(number):
+    return number * 2
+
+def fetch_and_process_number():
+    number = get_number()
+    return process_number(number)
