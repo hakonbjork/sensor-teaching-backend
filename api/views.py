cb2b27349cfddee2ceb235b8916b633ed2af57f7
Håkon Bjørkum <hakon1307@gmail.comm>
2024-01-23 14:41:45 +0100

    only make dummy data change one state per call

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from random import choice
+import random
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -16,11 +16,21 @@ def number(request):
     number = fetch_and_process_number()
     return Response({'number': number})
 
+STATES = ['flow', 'stressed', 'disengaged', 'sensor issues']
+IDS = range(1, 11)
+
+# Initialize with a default state for each ID
+random_states = [{'id': id, 'state': random.choice(STATES)} for id in IDS]
+
 @api_view(['GET'])
 def state_view(request):
-    STATES = ['flow', 'stressed', 'disengaged', 'sensor issues']
-    IDS = range(1, 11)
-    random_states = [{'id': id, 'state': choice(STATES)} for id in IDS]
+    # Select a random ID
+    random_id = random.choice(IDS)
+    # Update the state of the selected ID
+    for state in random_states:
+        if state['id'] == random_id:
+            state['state'] = random.choice(STATES)
+            break
     
     serializer = StateSerializer(random_states, many=True)
     return Response(serializer.data)
